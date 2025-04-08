a7962ecefc962d4c3de6ed4986df674e28b300a0
Franck-crassava <113307257+Franck-crassava@users.noreply.github.com>
2025-04-08 13:45:35 +0200

    Update password-generator.py

--- password-generator.py
+++ password-generator.py
@@ -1,17 +1,17 @@
 import string
 import sys
-import secrets  # Utilisation du module secrets pour une génération plus sécurisée
+import secrets  
 
-# Fonction de génération de mot de passe
+
 def generate_password(length=12, use_digits=True, use_special=True, exclude_chars="", use_secure=True):
-    # Caractères possibles pour le mot de passe
-    characters = string.ascii_letters  # Lettres majuscules et minuscules
+
+    characters = string.ascii_letters  
     
     if use_digits:
-        characters += string.digits  # Ajoute les chiffres
+        characters += string.digits  
     
     if use_special:
-        characters += string.punctuation  # Ajoute les symboles
+        characters += string.punctuation  
     
     # Exclure des caractères spécifiques
     characters = ''.join(c for c in characters if c not in exclude_chars)
@@ -24,14 +24,14 @@ def generate_password(length=12, use_digits=True, use_special=True, exclude_char
     
     return password
 
-# Fonction principale
+
 def main():
-    # Valeurs par défaut
+
     length = 12
     use_digits = True
     use_special = True
     exclude_chars = ""  # Liste des caractères à exclure
-    use_secure = True  # Par défaut, utilisation de secrets
+    use_secure = True  # Utilisation de secrets
     
     # Lecture des arguments en ligne de commande
     if len(sys.argv) > 1:
@@ -54,7 +54,7 @@ def main():
         if len(sys.argv) > 5:
             use_secure = sys.argv[5].lower() in ['true', '1', 'yes']
     
-    # Générer le mot de passe
+    # Génération du mot de passe
     password = generate_password(length, use_digits, use_special, exclude_chars, use_secure)
     
     # Afficher ou sauvegarder le mot de passe
